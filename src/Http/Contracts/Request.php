71557a1348c0d9b198e1abeda902f0d9416a9522
risan <risanbagja@gmail.com>
2016-02-25 15:51:40 +0100

    Improve Request class.

--- src/Http/Contracts/Request.php
+++ src/Http/Contracts/Request.php
@@ -8,5 +8,35 @@ interface Request
 
     public function method();
 
+    public function root();
+
+    public function url();
+
+    public function fullUrl();
+
     public function path();
+
+    public function ip();
+
+    public function ips();
+
+    public function isAjax();
+
+    public function isJson();
+
+    public function contentType();
+
+    public function header($key, $default = null);
+
+    public function query($key, $default = null);
+
+    public function cookie($key, $default = null);
+
+    public function content();
+
+    public function json($key = null, $default = null);
+
+    public function input($key = null, $default = null);
+
+    public function inputSource();
 }
--- src/Http/Request.php
+++ src/Http/Request.php
@@ -2,11 +2,14 @@
 
 namespace Minimal\Http;
 
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Minimal\Http\Contracts\Request as RequestContract;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class Request extends SymfonyRequest implements RequestContract
 {
+    protected $json;
+
     public static function capture()
     {
         return static::createFromGlobals();
@@ -17,8 +20,129 @@ class Request extends SymfonyRequest implements RequestContract
         return $this->getMethod();
     }
 
+    public function root()
+    {
+        $url = $this->getSchemeAndHttpHost() . $this->getBaseUrl();
+
+        return rtrim($url, '/');
+    }
+
+    public function url()
+    {
+        $url = preg_replace('/\?.*/', '', $this->getUri());
+
+        return rtrim($url, '/');
+    }
+
+    public function fullUrl()
+    {
+        $query = $this->getQueryString();
+
+        $question = $this->getBaseUrl() . $this->getPathInfo() === '/' ? '/?' : '?';
+
+        return $query ? $this->url() . $question . $query : $this->url();
+    }
+
     public function path()
     {
         return $this->getPathInfo();
+    }
+
+    public function ip()
+    {
+        return $this->getClientIp();
+    }
+
+    public function ips()
+    {
+        return $this->getClientIps();
+    }
+
+    public function isAjax()
+    {
+        return $this->isXmlHttpRequest();
+    }
+
+    public function isJson()
+    {
+        $contentType = $this->contentType();
+
+        if (strpos($contentType, '/json') !== false) {
+            return true;
+        }
+
+        if (strpos($contentType, '+json') !== false) {
+            return true;
+        }
+
+       return false;
+    }
+
+    public function contentType()
+    {
+        return $this->header('CONTENT_TYPE');
+    }
+
+    public function header($key, $default = null)
+    {
+        return $this->headers->get($key, $default);
+    }
+
+    public function query($key, $default = null)
+    {
+        return $this->query->get($key, $default);
+    }
+
+    public function cookie($key, $default = null)
+    {
+        return $this->cookies->get($key, $default);
+    }
+
+    public function content()
+    {
+        return $this->getContent();
+    }
+
+    public function json($key = null, $default = null)
+    {
+        if ( ! isset($this->json)) {
+            $this->json = new ParameterBag((array) json_decode($this->content(), true));
+        }
+
+        if (is_null($key)) {
+            return $this->json;
+        }
+
+        return $this->json->get($key, $default);
+    }
+
+    public function input($key = null, $default = null)
+    {
+        $inputSource = $this->inputSource();
+
+        if (is_null($key)) {
+            return $inputSource;
+        }
+
+        if (is_string($key)) {
+            return $inputSource->get($key, $default);
+        }
+
+        $inputs = [];
+
+        foreach ($key as $inputKey) {
+            array_push($inputs, $inputSource->get($inputKey));
+        }
+
+        return $inputs;
+    }
+
+    public function inputSource()
+    {
+        if ($this->isJson()) {
+            return $this->json();
+        }
+
+        return $this->method() === 'GET' ? $this->query : $this->request;
     }
 }
